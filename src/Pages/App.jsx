a3d5944af15d573d1e4fdd6a88ac4fa66396b070
Aniket Patel <mainavvideo@gmail.com>
2026-07-15 15:16:06 +0530

    Describe your changes

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../component/Navbar/Navbar.jsx'
-
+import { Outlet } from "react-router-dom";
+import Navbar from "../component/Navbar/Navbar.jsx";
 
 function App() {
+  const role = localStorage.getItem("role");
 
   return (
     <>
       <Navbar />
+{role === "admin" && (
+      <div className="admin-main">
+        <div className="admin-content">
+          
+        <div className="admin-list">
+          👑 Admin Logged In
+        </div>
+     
+        </div>
+      </div>
+       )}
       <Outlet />
       <footer></footer>
     </>
-  )
+  );
 }
 
 export default App;
