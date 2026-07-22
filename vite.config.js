cb2e6d1d5e66228f55cd84b38aeb6bc864a052e3
Aniket Patel <mainavvideo@gmail.com>
2026-07-22 20:44:40 +0530

    Describe your changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/HD-VID/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
 })
